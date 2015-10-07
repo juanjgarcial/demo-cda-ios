00cd2bcbaf5ebecc41fbb170075cc459c51ce882
Juan Garcia <juan.garcia@synergy-gb.com>
2015-10-07 09:40:37 -0500

    Agregagando pantalla de contacto

---
+++ DemoCDA/ContactViewController.h
@@ -0,0 +1,13 @@
+//
+//  ContactViewController.h
+//  DemoCDA
+//
+//  Created by SGBPty-002 on 10/7/15.
+//  Copyright © 2015 Synergy Global Business. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ContactViewController : UIViewController
+
+@end
