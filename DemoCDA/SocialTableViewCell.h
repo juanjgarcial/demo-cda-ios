848a620304ab6882be781ad5df409315fceb8045
Juan Garcia <juan.garcia@synergy-gb.com>
2015-10-07 09:40:29 -0500

    Agregagando pantalla de contacto

---
+++ DemoCDA/SocialTableViewCell.h
@@ -0,0 +1,15 @@
+//
+//  SocialTableViewCell.h
+//  DemoCDA
+//
+//  Created by SGBPty-002 on 10/7/15.
+//  Copyright © 2015 Synergy Global Business. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SocialTableViewCell : UITableViewCell
+
+@property (weak, nonatomic) IBOutlet UILabel *socialTitle;
+@property (weak, nonatomic) IBOutlet UIImageView *socialImage;
+@end
---
+++ DemoCDA/SocialTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  SocialTableViewCell.m
+//  DemoCDA
+//
+//  Created by SGBPty-002 on 10/7/15.
+//  Copyright © 2015 Synergy Global Business. All rights reserved.
+//
+
+#import "SocialTableViewCell.h"
+
+@implementation SocialTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
